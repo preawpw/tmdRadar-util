b377fa7d399de18a3a8072954473c23194509205
preapw <Amq-ph@hotmail.com>
2017-11-14 14:18:50 +0700

    update

--- Radar.js
+++ Radar.js
@@ -1,19 +1,3 @@
-var app = require('express')();
-var radar = require('./lib/Radar_IMG');
-var avg = require('./lib/Radar_IMG/AVGfunction.js');
-var port = process.env.PORT || 7777;
-app.get("/currentimage", function(req, res) {
-    radar.radar_callback("http://203.155.220.231/Radar/pics/zfiltered.jpg","currentRadar.png", function(err,img){       
-        res.type('png');
-        res.send(img);       
-    });
-});
-app.get("/avg", function(req, res) {
-    avg.avg_callback(req.query.px, req.query.py, function(err,avg){ 
-        res.setHeader('Access-Control-Allow-Origin','*');  
-        res.send(avg);             
-    });
-});
-app.listen(port, function() {
-    console.log('Starting node.js on port ' + port);
+var radar = require('./lib/Radar_IMG/');
+    radar.radar_callback( function(err,img){       
 });
